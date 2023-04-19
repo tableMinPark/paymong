7e7f4e71d23b97932e9fb6ca3beba4065a7c2f28
nayu99 <nyj3230@naver.com>
2023-04-20 01:12:48 +0900

    NewBranch (BE)feature/eureka

---
+++ back-end/information/src/main/java/com/paymong/information/information/controller/InformationController.java
@@ -0,0 +1,14 @@
+package com.paymong.information.information.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/information")
+public class InformationController {
+
+}
---
+++ back-end/paypoint/src/main/java/com/paymong/paypoint/PaypointApplication.java
@@ -0,0 +1,13 @@
+package com.paymong.paypoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaypointApplication.class, args);
+    }
+
+}

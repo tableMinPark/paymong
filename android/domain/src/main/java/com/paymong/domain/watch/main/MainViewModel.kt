c89de31d58bf5205b3f60a349b90d10009e2c802
황수빈 <marybin99@naver.com>
2023-04-24 15:44:18 +0900

    Feauture : 랜딩 페이지 추가 및 메인화면 배경 추가

---
+++ android/domain/src/main/java/com/paymong/domain/watch/main/MainViewModel.kt
@@ -0,0 +1,23 @@
+package com.paymong.domain.watch.main
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    var background by mutableStateOf("")
+
+    private lateinit var load : Job
+
+    init {
+        if(::load.isInitialized) load.cancel()
+
+        load = viewModelScope.launch {
+            background = "BG100"
+        }
+    }
+}

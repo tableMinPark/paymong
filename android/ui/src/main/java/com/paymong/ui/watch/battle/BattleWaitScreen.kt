d37192d217f1fe7d3db4b36ccfa26e1c956126fd
황수빈 <marybin99@naver.com>
2023-04-24 18:00:13 +0900

    Feauture : 배틀화면

--- android/ui/src/main/java/com/paymong/ui/watch/battle/BattleWaitScreen.kt
+++ android/ui/src/main/java/com/paymong/ui/watch/battle/BattleWaitScreen.kt
@@ -1,14 +1,21 @@
 package com.paymong.ui.watch.battle
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -16,38 +23,52 @@ import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
 import com.paymong.common.code.AnimationCode
 import com.paymong.common.navigation.WatchNavItem
+import com.paymong.ui.R
 import com.paymong.ui.theme.PaymongTheme
+import com.paymong.ui.theme.dalmoori
 import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun BattleWait(
     navController: NavHostController
 ) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxHeight()
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
+    val img = painterResource(R.drawable.main_bg)
+    Box{
+        Image(painter = img, contentDescription = null, contentScale = ContentScale.Crop)
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxHeight()
         ) {
-            Text(text = "...배틀을 찾는 중...", textAlign = TextAlign.Center)
-        }
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "기기를 태그 해주세요.", textAlign = TextAlign.Center)
-        }
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Button(
-                onClick = { navController.navigate(WatchNavItem.BattleFind.route) },
+            Row(
+                horizontalArrangement = Arrangement.Center,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Text(text = "다음으로")
+                Text(
+                    text = "...배틀을 찾는 중...",
+                    textAlign = TextAlign.Center,
+                    fontFamily = dalmoori,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(vertical = 20.dp),
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "기기를 태그 해주세요.", textAlign = TextAlign.Center,
+                        fontFamily = dalmoori,)
+            }
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = { navController.navigate(WatchNavItem.BattleFind.route) },
+                    modifier = Modifier.height(45.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent)
+                ) {
+                    Text(text = "다음")
+                }
             }
         }
     }

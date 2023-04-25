b96977efc16bc73cb4299b6ddca2134e96ec3991
rudgns <rudgns8285@naver.com>
2023-04-26 03:53:16 +0900

    Feature : (BE)management/feed [진행중]

---
+++ backend/management/src/main/java/com/paymong/management/feed/controller/FeedController.java
@@ -0,0 +1,67 @@
+package com.paymong.management.feed.controller;
+
+import com.paymong.management.feed.dto.FeedFoodReqDto;
+import com.paymong.management.feed.dto.FeedSnackReqDto;
+import com.paymong.management.feed.service.FeedService;
+import com.paymong.management.feed.vo.FeedFoodReqVo;
+import com.paymong.management.feed.vo.FeedSnackReqVo;
+import com.paymong.management.global.code.ErrorCode;
+import com.paymong.management.global.dto.ErrorDto;
+import com.paymong.management.global.dto.ResDto;
+import com.paymong.management.global.exception.NotFoundActionException;
+import com.paymong.management.global.exception.NotFoundMongException;
+import com.paymong.management.mong.controller.MongController;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/management/feed")
+public class FeedController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FeedController.class);
+    private final FeedService feedService;
+    /* 음식 먹이기 */
+    @PostMapping("/food")
+    public ResponseEntity<Object> feedFood(FeedFoodReqDto feedFoodReqDto) throws Exception{
+        FeedFoodReqVo feedFoodReqVo = new FeedFoodReqVo(feedFoodReqDto);
+        try {
+            feedService.feedFood(feedFoodReqVo);
+            return ResponseEntity.status(HttpStatus.OK).body(new ResDto("SUCCESS"));
+        }catch (NullPointerException e){
+            LOGGER.info("code : {}, message : {}", ErrorCode.NULL_POINT.getCode(), ErrorCode.NULL_POINT.name());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto(ErrorCode.NULL_POINT));
+        }catch (NotFoundMongException e){
+            LOGGER.info("code : {}, message : {}", ErrorCode.NOT_FOUND.getCode(), ErrorCode.NOT_FOUND.name());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto(ErrorCode.NOT_FOUND));
+        }catch (NotFoundActionException e){
+            LOGGER.info("code : {}, message : {}", ErrorCode.NOT_ACTION.getCode(), ErrorCode.NOT_ACTION.name());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto(ErrorCode.NOT_ACTION));
+        }
+    }
+
+    /* 간식 먹이기 */
+    @PostMapping("/snack")
+    public ResponseEntity<Object> feedSnack(FeedSnackReqDto feedSnackReqDto) throws Exception{
+        FeedSnackReqVo feedSnackReqVo = new FeedSnackReqVo(feedSnackReqDto);
+        try {
+            feedService.feedSnack(feedSnackReqVo);
+            return ResponseEntity.status(HttpStatus.OK).body(new ResDto("SUCCESS"));
+        }catch (NullPointerException e){
+            LOGGER.info("code : {}, message : {}", ErrorCode.NULL_POINT.getCode(), ErrorCode.NULL_POINT.name());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto(ErrorCode.NULL_POINT));
+        }catch (NotFoundMongException e){
+            LOGGER.info("code : {}, message : {}", ErrorCode.NOT_FOUND.getCode(), ErrorCode.NOT_FOUND.name());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto(ErrorCode.NOT_FOUND));
+        }catch (NotFoundActionException e){
+            LOGGER.info("code : {}, message : {}", ErrorCode.NOT_ACTION.getCode(), ErrorCode.NOT_ACTION.name());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto(ErrorCode.NOT_ACTION));
+        }
+    }
+}
---
+++ backend/management/src/main/java/com/paymong/management/feed/service/FeedService.java
@@ -0,0 +1,84 @@
+package com.paymong.management.feed.service;
+
+import com.paymong.management.feed.vo.FeedFoodReqVo;
+import com.paymong.management.feed.vo.FeedSnackReqVo;
+import com.paymong.management.global.exception.NotFoundActionException;
+import com.paymong.management.global.exception.NotFoundMongException;
+import com.paymong.management.mong.entity.Mong;
+import com.paymong.management.mong.repository.MongRepository;
+import com.paymong.management.status.entitiy.Status;
+import com.paymong.management.status.repository.StatusRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class FeedService {
+    private final MongRepository mongRepository;
+    private final StatusRepository statusRepository;
+
+    @Transactional
+    public void feedFood(FeedFoodReqVo feedFoodReqVo) throws Exception{
+        if(feedFoodReqVo.getFoodCode() == null){
+            throw new NullPointerException();
+        }
+        // auth에서 mongId 받아오기
+
+        // mongId로 해당 mong 찾기
+        Mong mong = mongRepository.findByMongId(1L)
+                .orElseThrow(() -> new NotFoundMongException());
+
+        // foodCode에 따라 해당 액션 찾기
+        String foodCode = feedFoodReqVo.getFoodCode().substring(2);
+        int code = Integer.parseInt(foodCode);
+
+        Status status = null;
+        if(code == 0){
+            status = statusRepository.findByCode("AT000")
+                    .orElseThrow(()->new NotFoundActionException());
+        }else if(1<=code && 3>=code){
+            status = statusRepository.findByCode("AT010")
+                    .orElseThrow(()->new NotFoundActionException());
+        }else if(4<=code && 6>=code){
+            status = statusRepository.findByCode("AT020")
+                    .orElseThrow(()->new NotFoundActionException());
+        }else{
+            status = statusRepository.findByCode("AT030")
+                    .orElseThrow(()->new NotFoundActionException());
+        }
+
+        mong.setWeight(mong.getWeight() + status.getWeight());
+
+    }
+
+    @Transactional
+    public void feedSnack(FeedSnackReqVo feedSnackReqVo) throws Exception{
+        if(feedSnackReqVo.getSnackCode() == null){
+            throw new NullPointerException();
+        }
+        // auth에서 mongId 받아오기
+
+        // mongId로 해당 mong 찾기
+        Mong mong = mongRepository.findByMongId(1L)
+                .orElseThrow(() -> new NotFoundMongException());
+
+        // foodCode에 따라 해당 액션 찾기
+        String foodCode = feedSnackReqVo.getSnackCode().substring(2);
+        int code = Integer.parseInt(foodCode);
+
+        Status status = null;
+        if(code>= 0 && code<= 3){
+            status = statusRepository.findByCode("AT001")
+                    .orElseThrow(()->new NotFoundActionException());
+        }else{
+            status = statusRepository.findByCode("AT011")
+                    .orElseThrow(()->new NotFoundActionException());
+        }
+
+        mong.setWeight(mong.getWeight() + status.getWeight());
+        mong.setHealth(mong.getHealth() + status.getHealth());
+        mong.setStrength(mong.getStrength() + status.getStrength());
+    }
+}

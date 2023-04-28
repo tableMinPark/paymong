f711d8a3572afa44fd96aac8145ba09586a22921
kwonhyeokgeun <xkhg0611x@naver.com>
2023-04-28 10:29:03 +0900

    feature : test

--- backend/paypoint/src/main/java/com/paymong/paypoint/paypoint/controller/PaypointController.java
+++ backend/paypoint/src/main/java/com/paymong/paypoint/paypoint/controller/PaypointController.java
@@ -3,6 +3,7 @@ package com.paymong.paypoint.paypoint.controller;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/paypoint")
 public class PaypointController {
-    @PostMapping("/")
-    void a(){
-
+    @GetMapping("/")
+    public void a(){
+        System.out.println("paypoint test!!!");
     }
 
 }

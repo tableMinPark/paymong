92322ab4fa149d0211e12910f81f392dd8ecf767
nayu99 <nyj3230@gmail.com>
2023-04-29 22:38:07 +0900

    Feature : 공통코드 리스트 조회 추가

---
+++ backend/common/src/main/java/com/paymong/common/common/service/CommonService.java
@@ -0,0 +1,36 @@
+package com.paymong.common.common.service;
+
+import com.paymong.common.common.dto.request.FindAllCommonCodeReqDto;
+import com.paymong.common.common.dto.response.FindAllCommonCodeResDto;
+import com.paymong.common.common.entity.CommonCode;
+import com.paymong.common.common.entity.GroupCode;
+import com.paymong.common.common.repository.CommonCodeRepository;
+import com.paymong.common.common.repository.GroupCodeRepository;
+import com.paymong.common.global.exception.NotFoundException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CommonService {
+
+    private final CommonCodeRepository commonCodeRepository;
+
+    private final GroupCodeRepository groupCodeRepository;
+
+    @Transactional
+    public FindAllCommonCodeResDto findAllCommonCode(
+        FindAllCommonCodeReqDto findAllCommonCodeReqDto) throws RuntimeException {
+
+        GroupCode groupCode = groupCodeRepository.findById(findAllCommonCodeReqDto.getGroupCode())
+            .orElseThrow(() -> new NotFoundException());
+        List<CommonCode> commonCodeList = commonCodeRepository.findAllByGroupCode(groupCode)
+            .orElseThrow();
+        return FindAllCommonCodeResDto.builder().commonCodeVoList(commonCodeList).build();
+    }
+
+}

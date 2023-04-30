461b017d9740b3a835a7a511364e456b140b852b
subin <marybin99@naver.com>
2023-04-30 21:56:45 +0900

    Feature : PayMong 컬렉션

--- android/ui/src/main/java/com/paymong/ui/app/collect/CollectPayMongScreen.kt
+++ android/ui/src/main/java/com/paymong/ui/app/collect/CollectPayMongScreen.kt
@@ -1,12 +1,23 @@
 package com.paymong.ui.app.collect
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,6 +26,9 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.android.volley.Header
+import com.paymong.common.R
+import com.paymong.common.code.AnimationCode
 import com.paymong.common.navigation.AppNavItem
 import com.paymong.domain.app.collect.CollectPayMongViewModel
 import com.paymong.ui.app.component.TopBar
@@ -30,10 +44,40 @@ fun CollectPayMong(navController: NavController) {
 }
 
 @Composable
+fun PayMongHeader(title:String, open: Boolean, setIsOpen:(Boolean)->Unit){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(35.dp)
+            .background(color = Color.White.copy(alpha = 0.5f)),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        var icon = 0
+        Text(title, fontFamily = dalmoori, fontSize = 20.sp, color = Color.White, modifier = Modifier.padding(start = 20.dp))
+        icon = if(open){
+            R.drawable.close
+        } else{ //status == close
+            R.drawable.open
+        }
+        Image(painterResource(icon), contentDescription = null
+            , modifier = Modifier
+                .padding(end = 20.dp)
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = { setIsOpen(!open) }
+                ))
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
 fun CollectPayMongUI(
     navController: NavController,
     viewModel: CollectPayMongViewModel
 ) {
+    val isOpen by remember { mutableStateOf(true) }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
@@ -43,11 +87,20 @@ fun CollectPayMongUI(
             backgroundColor = PayMongNavy
         ) {
             Box(Modifier.padding(it)) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Text(text = "본문", textAlign = TextAlign.Center)
+                val isOpen = remember { mutableStateOf(true) }
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth()
+                ){
+//                    var isOpen = true
+                    stickyHeader {
+                        PayMongHeader("알", isOpen.value, setIsOpen = { isOpen.value = it })
+                    }
+                    if(isOpen.value){
+                        items(50){
+                            Text(text = "알")
+                        }
+                    }
+
                 }
             }
         }

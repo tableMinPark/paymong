7b440c7d9af36c33c9003a7a0cbaa384abac076d
youmean0427 <youmean0427@naver.com>
2023-04-30 20:15:13 +0900

    Feature : Feed UI 반응형 구현

--- android/ui/src/main/java/com/paymong/ui/watch/feed/FeedScreen.kt
+++ android/ui/src/main/java/com/paymong/ui/watch/feed/FeedScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,6 +25,8 @@ import com.paymong.domain.watch.feed.FeedViewModel
 import com.paymong.common.R
 import com.paymong.ui.theme.PaymongTheme
 import com.paymong.ui.theme.dalmoori
+import com.paymong.ui.watch.activity.BigWatch
+import com.paymong.ui.watch.activity.SmallWatch
 
 @Composable
 fun Feed(navController: NavHostController) {
@@ -36,8 +39,99 @@ fun FeedUI(
     navController: NavHostController,
     viewModel: FeedViewModel
 ) {
+
+
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp
+    val screenHeightDp = configuration.screenHeightDp
+
     val img = painterResource(R.drawable.main_bg)
     Image(painter = img, contentDescription = null, contentScale = ContentScale.Crop)
+
+
+    if (screenWidthDp < 200) {
+        SmallWatch( navController, viewModel)
+    }
+    else {
+        BigWatch( navController, viewModel)
+    }
+
+
+}
+
+
+
+@Preview(device = Devices.WEAR_OS_LARGE_ROUND, showSystemUi = true)
+@Composable
+fun FeedPreview() {
+    val navController = rememberSwipeDismissableNavController()
+    PaymongTheme {
+        Feed(navController)
+    }
+}
+
+@Composable
+fun SmallWatch(    navController: NavHostController,
+                   viewModel: FeedViewModel) {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxHeight()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(
+                onClick = { navController.navigate(WatchNavItem.FeedBuyList.route + "/RICE") },
+                modifier = Modifier.size(width = 200.dp, height = 95.dp),
+                shape = RectangleShape,
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent)
+            ) {
+                Text(text = "밥",
+                    fontFamily = dalmoori,
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(top= 20.dp))
+            }
+        }
+        // * Line *
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ){
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .height(5.dp))
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+
+                .fillMaxWidth()
+                .padding(top = 5.dp)
+        ) {
+            Button(
+                onClick = { navController.navigate(WatchNavItem.FeedBuyList.route + "/SNACK") },
+                modifier = Modifier.size(width = 200.dp, height = 95.dp),
+                shape = RectangleShape,
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent)
+            ) {
+                Text(text = "간식",
+                    fontFamily = dalmoori,
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(bottom= 20.dp))
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun BigWatch(    navController: NavHostController,
+                   viewModel: FeedViewModel) {
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -83,16 +177,5 @@ fun FeedUI(
 
             }
         }
-    }
-}
-
-
-
-@Preview(device = Devices.WEAR_OS_LARGE_ROUND, showSystemUi = true)
-@Composable
-fun FeedPreview() {
-    val navController = rememberSwipeDismissableNavController()
-    PaymongTheme {
-        Feed(navController)
     }
 }

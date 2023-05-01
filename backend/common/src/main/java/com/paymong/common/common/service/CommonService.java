6c3a7001059147d53e19b870c25cb0551871fa51
nayu99 <nyj3230@gmail.com>
2023-05-01 15:12:32 +0900

    Fix : 충돌 해결

--- backend/common/src/main/java/com/paymong/common/common/service/CommonService.java
+++ backend/common/src/main/java/com/paymong/common/common/service/CommonService.java
@@ -6,6 +6,7 @@ import com.paymong.common.common.dto.request.FindCommonCodeReqDto;
 import com.paymong.common.common.dto.response.FindAllCommonCodeResDto;
 import com.paymong.common.common.dto.response.FindAllFoodResDto;
 import com.paymong.common.common.dto.response.FindCommonCodResDto;
+import com.paymong.common.common.dto.response.FindEggResDto;
 import com.paymong.common.common.dto.response.Food;
 import com.paymong.common.common.entity.CommonCode;
 import com.paymong.common.common.entity.GroupCode;
@@ -16,6 +17,7 @@ import com.paymong.common.global.exception.NotFoundException;
 import com.paymong.common.global.vo.request.FindLastBuyReqVo;
 import com.paymong.common.global.vo.response.FindLastBuyResVo;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -74,6 +76,15 @@ public class CommonService {
             }).collect(Collectors.toList()));
 
         return findAllFoodResDto;
+    }
+
+    @Transactional
+    public FindEggResDto findRandomEgg() throws RuntimeException {
+        List<CommonCode> commonCodeList = commonCodeRepository.findByCodeStartsWith("CH0")
+            .orElseThrow();
+        Random random = new Random(); //랜덤 객체 생성(디폴트 시드값 : 현재시간)
+        random.setSeed(System.currentTimeMillis()); //시드값 설정을 따로 할수도 있음
+        return FindEggResDto.of(commonCodeList.get(random.nextInt(commonCodeList.size())));
     }
 
 

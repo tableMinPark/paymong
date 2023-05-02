1d0b667bcb422ac005193fc76840a6a65ea505b9
kwonhyeokgeun <xkhg0611x@naver.com>
2023-05-02 22:02:38 +0900

    Feature : 어노테이션 추가

--- backend/auth/src/main/java/com/paymong/auth/auth/dto/request/LoginReqDto.java
+++ backend/auth/src/main/java/com/paymong/auth/auth/dto/request/LoginReqDto.java
@@ -1,10 +1,14 @@
 package com.paymong.auth.auth.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginReqDto {
     private String email;
     private String password;

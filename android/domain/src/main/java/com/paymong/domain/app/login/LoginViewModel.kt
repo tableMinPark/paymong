6811c7d22a2a199331de931dbe873a8705da6694
subin <marybin99@naver.com>
2023-05-03 00:44:44 +0900

    Fix : Login

--- android/domain/src/main/java/com/paymong/domain/app/login/LoginViewModel.kt
+++ android/domain/src/main/java/com/paymong/domain/app/login/LoginViewModel.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.launch
 
 class LoginViewModel(application : Application) : AndroidViewModel(application) {
     var id by mutableStateOf("")
+    var isClicked by mutableStateOf(false)
     private lateinit var load : Job
     private val loginRepository: LoginRepository = LoginRepository()
 
@@ -21,9 +22,13 @@ class LoginViewModel(application : Application) : AndroidViewModel(application) 
         if(::load.isInitialized) load.cancel()
 
         load = viewModelScope.launch {
+            isClicked = false
             val sharedPref = application.getSharedPreferences("loginId", Context.MODE_PRIVATE)
             id = sharedPref.getString("loginId","").toString()
 
+            if(isClicked){
+
+            }
             loginRepository.registerLogin(id)
         }
     }

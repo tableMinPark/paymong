ed46c8df587636c77fc18b9c879600f4e721ac7f
rudgns9334 <rudgns9334@gmail.com>
2023-05-02 16:41:54 +0900

    Feature : FeignClient 연동 중

--- backend/management/src/main/java/com/paymong/management/feed/service/FeedService.java
+++ backend/management/src/main/java/com/paymong/management/feed/service/FeedService.java
@@ -1,14 +1,18 @@
 package com.paymong.management.feed.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paymong.management.feed.vo.FeedFoodReqVo;
 import com.paymong.management.feed.vo.FeedSnackReqVo;
+import com.paymong.management.global.client.CommonServiceClient;
 import com.paymong.management.global.exception.NotFoundActionException;
 import com.paymong.management.global.exception.NotFoundMongException;
 import com.paymong.management.mong.entity.Mong;
 import com.paymong.management.mong.repository.MongRepository;
-import com.paymong.management.status.entitiy.Status;
-import com.paymong.management.status.repository.StatusRepository;
+import com.paymong.management.status.dto.FindStatusReqDto;
+import com.paymong.management.status.dto.FindStatusResDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,34 +21,35 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class FeedService {
     private final MongRepository mongRepository;
-    private final StatusRepository statusRepository;
+    private final CommonServiceClient commonServiceClient;
 
     @Transactional
     public void feedFood(FeedFoodReqVo feedFoodReqVo) throws Exception{
         // auth에서 mongId 받아오기
 
         // mongId로 해당 mong 찾기
-        Mong mong = mongRepository.findByMongId(1L)
+        Mong mong = mongRepository.findByMongId(feedFoodReqVo.getMongId())
                 .orElseThrow(() -> new NotFoundMongException());
 
         // foodCode에 따라 해당 액션 찾기
         String foodCode = feedFoodReqVo.getFoodCode().substring(2);
         int code = Integer.parseInt(foodCode);
 
-        Status status = null;
+        FindStatusReqDto findStatusReqDto = new FindStatusReqDto();
         if(code == 0){
-            status = statusRepository.findByCode("AT000")
-                    .orElseThrow(()->new NotFoundActionException());
+            findStatusReqDto.setCode("AT000");
         }else if(1<=code && 3>=code){
-            status = statusRepository.findByCode("AT010")
-                    .orElseThrow(()->new NotFoundActionException());
+            findStatusReqDto.setCode("AT010");
         }else if(4<=code && 6>=code){
-            status = statusRepository.findByCode("AT020")
-                    .orElseThrow(()->new NotFoundActionException());
+            findStatusReqDto.setCode("AT020");
         }else{
-            status = statusRepository.findByCode("AT030")
-                    .orElseThrow(()->new NotFoundActionException());
+            findStatusReqDto.setCode("AT030");
         }
+
+        ObjectMapper om = new ObjectMapper();
+        ResponseEntity<Object> response = commonServiceClient.findStatus(findStatusReqDto);
+        if(response.getStatusCode()== HttpStatus.BAD_REQUEST) throw new NotFoundActionException();
+        FindStatusResDto status = om.convertValue(response.getBody(), FindStatusResDto.class);
 
         mong.setWeight(mong.getWeight() + status.getWeight());
 
@@ -55,21 +60,24 @@ public class FeedService {
         // auth에서 mongId 받아오기
 
         // mongId로 해당 mong 찾기
-        Mong mong = mongRepository.findByMongId(1L)
+        Mong mong = mongRepository.findByMongId(feedSnackReqVo.getMongId())
                 .orElseThrow(() -> new NotFoundMongException());
 
         // foodCode에 따라 해당 액션 찾기
         String foodCode = feedSnackReqVo.getSnackCode().substring(2);
         int code = Integer.parseInt(foodCode);
 
-        Status status = null;
+        FindStatusReqDto findStatusReqDto = new FindStatusReqDto();
         if(code>= 0 && code<= 3){
-            status = statusRepository.findByCode("AT001")
-                    .orElseThrow(()->new NotFoundActionException());
+            findStatusReqDto.setCode("AT001");
         }else{
-            status = statusRepository.findByCode("AT011")
-                    .orElseThrow(()->new NotFoundActionException());
+            findStatusReqDto.setCode("AT011");
         }
+
+        ObjectMapper om = new ObjectMapper();
+        ResponseEntity<Object> response = commonServiceClient.findStatus(findStatusReqDto);
+        if(response.getStatusCode()== HttpStatus.BAD_REQUEST) throw new NotFoundActionException();
+        FindStatusResDto status = om.convertValue(response.getBody(), FindStatusResDto.class);
 
         mong.setWeight(mong.getWeight() + status.getWeight());
         mong.setHealth(mong.getHealth() + status.getHealth());

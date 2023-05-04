3ca5392fdf731559ae1b1b7dd641c4dfd8463638
tablemin_park <tablemin_park@daum.net>
2023-05-04 10:29:03 +0900

    Feature : api response intercepter 생성중

--- android/data/src/main/java/com/paymong/data/api/Api.kt
+++ android/data/src/main/java/com/paymong/data/api/Api.kt
@@ -6,6 +6,7 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.HTTP
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
@@ -28,10 +29,11 @@ interface Api {
                 .connectTimeout(TIMEOUT_LIMIT, TimeUnit.SECONDS)
                 .readTimeout(TIMEOUT_LIMIT, TimeUnit.SECONDS)
                 .writeTimeout(TIMEOUT_LIMIT, TimeUnit.SECONDS)
-                .addInterceptor(HeaderInterceptor())
+                .addInterceptor(AccessTokenInterceptor())
+//                .addInterceptor(AccessTokenExpireInterceptor())
                 .build()
         }
-        private class HeaderInterceptor : Interceptor {
+        private class AccessTokenInterceptor : Interceptor {
             @Throws(IOException::class)
             override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
                 val accessToken = DataApplication.prefs.getString("accessToken", "")
@@ -49,6 +51,32 @@ interface Api {
                 }
             }
         }
+
+//        private class AccessTokenExpireInterceptor : Interceptor {
+//            @Throws(IOException::class)
+//            override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+//                val response = chain.proceed(chain.request())
+//
+//                // 토큰 만료
+//                if (response.code() == 403) {
+//
+//                }
+//                // 권한 없음
+//                else if (response.code() == 401) {
+//                    // 패스
+//                }
+//
+//                return if (accessToken != null) {
+//                    val token = "Bearer $accessToken"
+//                    val newRequest = chain.request().newBuilder()
+//                        .addHeader("Authorization", token)
+//                        .build()
+//                    chain.proceed(newRequest)
+//                } else {
+//                    chain.proceed(chain.request())
+//                }
+//            }
+//        }
     }
 }
 

8d3ee4f8092a2535fe3ca67b7e33054e70155f9f
youmean0427 <youmean0427@naver.com>
2023-05-08 00:38:36 +0900

    Feature : MainInfo API 연결 / Poop UI 구현

--- android/ui/src/main/java/com/paymong/ui/watch/main/MainInfoScreen.kt
+++ android/ui/src/main/java/com/paymong/ui/watch/main/MainInfoScreen.kt
@@ -3,19 +3,21 @@ package com.paymong.ui.watch.main
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.wear.compose.material.Text
+
+import com.paymong.common.R
 import com.paymong.common.code.AnimationCode
 import com.paymong.common.code.CharacterCode
 import com.paymong.domain.watch.main.MainInfoViewModel
-import com.paymong.domain.watch.main.MainViewModel
+
 import com.paymong.ui.theme.PaymongTheme
 
 @Composable
@@ -31,41 +33,91 @@ fun MainInfoUI(
     animationState: MutableState<AnimationCode>,
     viewModel: MainInfoViewModel
 ) {
-    val mapViewModel : MainViewModel = viewModel()
+
     val configuration = LocalConfiguration.current
     val screenWidthDp = configuration.screenWidthDp
     var characterSize = 0
+    var poopSize = 0
+
 
 
     if (screenWidthDp < 200) {
-        characterSize = 120
-
+        characterSize = 100
+        poopSize = 25
 
     }
     else {
-        characterSize = 150
-
+        characterSize = 120
+        poopSize = 35
     }
+
+
 
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxHeight()
     ) {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = String.format("똥 개수 : %d", viewModel.poopCount), textAlign = TextAlign.Center)
-        }
-        Row(
+//        Row(
+//            horizontalArrangement = Arrangement.Center,
+//            modifier = Modifier.fillMaxWidth()
+//        ) {
+//            Text(text = String.format("맵 코드 : %s", mapViewModel.mapCode), textAlign = TextAlign.Center)
+//        }
+//        Row(
+//            horizontalArrangement = Arrangement.Center,
+//            modifier = Modifier.fillMaxWidth()
+//        ) {
+//            Text(text = String.format("똥 개수 : %d", viewModel.poopCount), textAlign = TextAlign.Center)
+//        }
+
+        Box(   modifier = Modifier.fillMaxWidth().fillMaxHeight(1f).padding(top=60.dp) ) {
+
+            Row(
                 horizontalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxWidth()
-        ) {
-            val character = painterResource(viewModel.mong.mongCode.resourceCode)
-            Image(painter = character, contentDescription = null, modifier = Modifier.width(characterSize.dp))
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+
+                val character = painterResource(viewModel.mong.mongCode.resourceCode)
+//                val character = painterResource(CharacterCode.CH302.resourceCode)
+                Image(
+                    painter = character,
+                    contentDescription = null,
+                    modifier = Modifier.width(characterSize.dp)
+                )
+//            Text(text = String.format("캐릭터 코드 : %s %s", animationState.value, viewModel.characterCode), textAlign = TextAlign.Center)
+            }
+
+            val poopCount = viewModel.poopCount
+
+            if (poopCount == 1) {
+                Poops(0, 100, 75, 0, poopSize)
+            }
+            else if (poopCount == 2) {
+                Poops(0, 100, 75, 0, poopSize)
+                Poops(90, 0, 80, 0, poopSize)
+            }
+            else if (poopCount == 3) {
+                Poops(0, 100, 75, 0, poopSize)
+                Poops(90, 0, 80, 0, poopSize)
+                Poops(30, 0, 92, 0, poopSize)
+            }
+            else if (poopCount == 4 ) {
+                Poops(0, 100, 75, 0, poopSize)
+                Poops(90, 0, 80, 0, poopSize)
+                Poops(30, 0, 92, 0, poopSize)
+                Poops(0, 60, 95, 0, poopSize)
+            }
+
+
+
+
+
         }
+
     }
 }
+
 
 @Preview(device = Devices.WEAR_OS_LARGE_ROUND, showSystemUi = true)
 @Composable
@@ -73,5 +125,26 @@ fun MainInfoPreview() {
     val animationState = remember { mutableStateOf(AnimationCode.Normal) }
     PaymongTheme {
         MainInfo(animationState)
+    }
+}
+
+
+@Composable
+fun Poops(start:Int, end:Int, top:Int, bottom:Int, poopSize:Int ){
+    val poops = painterResource(R.drawable.poops)
+
+    Box(
+//        horizontalArrangement = Arrangement.Center,
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxWidth()
+            .padding(start = start.dp, end = end.dp, top = top.dp, bottom = bottom.dp)
+    ) {
+
+
+        Image(
+            painter = poops,
+            contentDescription = null,
+            modifier = Modifier.size(poopSize.dp)
+        )
     }
 }

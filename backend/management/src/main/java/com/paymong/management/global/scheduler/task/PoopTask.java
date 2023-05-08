549c029caacabd63c391e1b6d517c270185d1d3b
rudgns9334 <rudgns9334@gmail.com>
2023-05-08 12:17:10 +0900

    Feature : Poop 스케줄러 추가

---
+++ backend/management/src/main/java/com/paymong/management/global/scheduler/task/PoopTask.java
@@ -0,0 +1,34 @@
+package com.paymong.management.global.scheduler.task;
+
+import com.paymong.management.global.exception.NotFoundMongException;
+import com.paymong.management.mong.entity.Mong;
+import com.paymong.management.mong.repository.MongRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PoopTask {
+
+    private final MongRepository mongRepository;
+    @Transactional
+    public void addPoop(Long mongId) throws NotFoundMongException {
+
+        Mong mong = mongRepository.findByMongId(mongId)
+                .orElseThrow(() -> new NotFoundMongException());
+        if(!mong.getActive()) throw new NotFoundMongException();
+        Integer poop = mong.getPoopCount();
+        Integer penalty = mong.getPenalty();
+        if(poop == 4){
+            // 패널티 적립
+            mong.setPenalty(penalty + 1);
+        }else{
+            // 갯수 적립
+            mong.setPoopCount(poop + 1);
+        }
+    }
+}
--- backend/management/src/main/java/com/paymong/management/poop/service/PoopService.java
+++ backend/management/src/main/java/com/paymong/management/poop/service/PoopService.java
@@ -26,20 +26,4 @@ public class PoopService {
         mong.setPoopCount(0);
     }
 
-    @Transactional
-    public void addPoop(Long mongId) throws NotFoundMongException {
-
-        Mong mong = mongRepository.findByMongId(mongId)
-                .orElseThrow(() -> new NotFoundMongException());
-        if(!mong.getActive()) throw new NotFoundMongException();
-        Integer poop = mong.getPoopCount();
-        Integer penalty = mong.getPenalty();
-        if(poop == 4){
-            // 패널티 적립
-            mong.setPenalty(penalty + 1);
-        }else{
-            // 갯수 적립
-            mong.setPoopCount(poop + 1);
-        }
-    }
 }

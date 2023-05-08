25dda37a56c0e1be7c791ab14d910e8cfbb63cac
황수빈 <marybin99@naver.com>
2023-05-08 15:32:03 +0900

    Feature : management api, viewmodel 정리

--- android/common/src/main/java/com/paymong/common/code/MongStateCode.kt
+++ android/common/src/main/java/com/paymong/common/code/MongStateCode.kt
@@ -4,5 +4,7 @@ enum class MongStateCode(
     val code: String,
     val codeName: String,
 ) {
-    CD000("CD000", "일반")
+    CD000("CD000", "일반"),
+    CD002("CD002","수면"),
+    CD004("CD004","배고픔")
 }

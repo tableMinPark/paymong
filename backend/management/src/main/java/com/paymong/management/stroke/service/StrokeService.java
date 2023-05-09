db6a25be6254a97d6d14795fc649de9418b33957
rudgns9334 <rudgns9334@gmail.com>
2023-05-09 20:20:57 +0900

    Feature :active history -쓰다듬기편-

--- backend/management/src/main/java/com/paymong/management/stroke/service/StrokeService.java
+++ backend/management/src/main/java/com/paymong/management/stroke/service/StrokeService.java
@@ -2,6 +2,8 @@ package com.paymong.management.stroke.service;
 
 import com.paymong.management.global.exception.NotFoundMongException;
 import com.paymong.management.global.scheduler.StrokeScheduler;
+import com.paymong.management.history.entity.ActiveHistory;
+import com.paymong.management.history.repository.ActiveHistoryRepository;
 import com.paymong.management.mong.entity.Mong;
 import com.paymong.management.mong.repository.MongRepository;
 import com.paymong.management.stroke.vo.StrokeMongReqVo;
@@ -9,13 +11,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class StrokeService {
     private final MongRepository mongRepository;
-
     private final StrokeScheduler strokeScheduler;
+    private final ActiveHistoryRepository activeHistoryRepository;
     @Transactional
     public Boolean strokeMong(StrokeMongReqVo strokeMongReqVo) throws Exception{
 
@@ -26,6 +29,15 @@ public class StrokeService {
         Mong mong = mongRepository.findByMongId(strokeMongReqVo.getMongId())
                 .orElseThrow(() -> new NotFoundMongException());
         mong.setStrokeCount(mong.getStrokeCount() + 1);
+
+        ActiveHistory activeHistory = ActiveHistory.builder()
+                .activeCode("AT006")
+                .activeTime(LocalDateTime.now())
+                .mongId(strokeMongReqVo.getMongId())
+                .build();
+
+        activeHistoryRepository.save(activeHistory);
+
         strokeScheduler.startScheduler(strokeMongReqVo.getMongId());
         return true;
     }

efcdda18ac6a6473d637963bfb251fe8ab721a61
tablemin_park <tablemin_park@daum.net>
2023-05-11 17:48:14 +0900

    Feature : 배틀 스텟 수정

--- backend/battle/src/main/java/com/paymong/battle/global/client/InformationServiceClient.java
+++ backend/battle/src/main/java/com/paymong/battle/global/client/InformationServiceClient.java
@@ -1,12 +1,14 @@
 package com.paymong.battle.global.client;
 
 import com.paymong.battle.battle.dto.request.FindMongBattleReqDto;
+import feign.Body;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "information")
 public interface InformationServiceClient {
     @GetMapping("/information/mong/status_battle")
-    ResponseEntity<Object> findMongBattle(FindMongBattleReqDto findMongBattleReqDto);
+    ResponseEntity<Object> findMongBattle(@SpringQueryMap FindMongBattleReqDto findMongBattleReqDto);
 }

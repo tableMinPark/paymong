43917b0c56718a6c822734f63dd100968307bb41
rudgns9334 <rudgns9334@gmail.com>
2023-05-12 10:47:50 +0900

    Fix : 몽 죽음시 history추가

--- backend/management/src/main/java/com/paymong/management/global/scheduler/task/DeathTask.java
+++ backend/management/src/main/java/com/paymong/management/global/scheduler/task/DeathTask.java
@@ -1,9 +1,12 @@
 package com.paymong.management.global.scheduler.task;
 
+import com.paymong.management.global.code.MongActiveCode;
 import com.paymong.management.global.code.MongConditionCode;
 import com.paymong.management.global.code.WebSocketCode;
 import com.paymong.management.global.exception.NotFoundMongException;
 import com.paymong.management.global.socket.service.WebSocketService;
+import com.paymong.management.history.entity.ActiveHistory;
+import com.paymong.management.history.repository.ActiveHistoryRepository;
 import com.paymong.management.mong.entity.Mong;
 import com.paymong.management.mong.repository.MongRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
@@ -19,11 +23,22 @@ public class DeathTask {
 
     private final MongRepository mongRepository;
     private final WebSocketService webSocketService;
+    private final ActiveHistoryRepository activeHistoryRepository;
+
     @Transactional
     public void deathMong(Long mongId) throws NotFoundMongException {
         Mong mong = mongRepository.findByMongIdAndActive(mongId, true)
                 .orElseThrow(() -> new NotFoundMongException());
         mong.setStateCode(MongConditionCode.DIE.getCode());
+
+        ActiveHistory activeHistory = ActiveHistory.builder()
+                .activeCode(MongConditionCode.DIE.getCode())
+                .activeTime(LocalDateTime.now())
+                .mongId(mongId)
+                .build();
+
+        activeHistoryRepository.save(activeHistory);
+
         log.info("{} 의 mong이 죽었습니다.", mongId);
         webSocketService.sendStatus(mong, WebSocketCode.DEATH);
     }
